96055fab8ba8b54d097faaac545e72aff57169c5
Vyshakhams <64062881+Vyshakhams@users.noreply.github.com>
2020-07-08 22:16:20 +0530

    Update settings.py
    Removed SECRET_KEY and made DEBUG = False

--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lc8*xsdl&z1uwz2nw52@4%fca*2av_9@!@@1^vxhhoo1mzapqw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
